c6b546baefdaaa22d2703783c27ff60245b4703e
Rexios <rexios@rexios.dev>
2023-01-24 18:10:13 -0500

    ...

--- bin/git_dep_check.dart
+++ bin/git_dep_check.dart
@@ -1,60 +1,81 @@
+import 'dart:io';
+
 import 'package:ansicolor/ansicolor.dart';
 import 'package:git_dep_check/git_dependency_reference.dart';
 import 'package:github/github.dart' hide GitReference;
 import 'package:pub_api_client/pub_api_client.dart';
-import 'package:pubspec/pubspec.dart';
+import 'package:yaml/yaml.dart';
 
 final magentaPen = AnsiPen()..magenta();
 final greenPen = AnsiPen()..green();
 final yellowPen = AnsiPen()..yellow();
 final redPen = AnsiPen()..red();
 
-void main() async {
-  final pub = PubClient();
-  final github = GitHub();
+final pub = PubClient();
+final github = GitHub();
 
-  final pubspec = await PubSpec.loadFile(
-      '../safeguard_flutter/safeguard_live/pubspec.yaml');
+void main() async {
+  final pubspec = await loadYaml(
+      File('../safeguard_flutter/safeguard_live/pubspec.yaml')
+          .readAsStringSync());
   final gitDependencies = {
+    ...filterGitDependencies('dependencies', pubspec['dependencies']),
+    ...filterGitDependencies('dev_dependencies', pubspec['dev_dependencies']),
     ...filterGitDependencies(
-      DependencyLocation.dependencies,
-      pubspec.dependencies,
-    ),
-    ...filterGitDependencies(
-      DependencyLocation.devDependencies,
-      pubspec.devDependencies,
-    ),
-    ...filterGitDependencies(
-      DependencyLocation.dependencyOverrides,
-      pubspec.dependencyOverrides,
+      'dependency_overrides',
+      pubspec['dependency_overrides'],
     ),
   }.values;
 
   for (final dependency in gitDependencies) {
-    await checkPrs(pubspec, dependency);
+    // await checkPrs(pubspec, dependency);
   }
 }
 
 Map<String, GitDependencyReference> filterGitDependencies(
-  DependencyLocation location,
-  Map<String, DependencyReference> dependencies,
+  String location,
+  YamlMap? dependencies,
 ) {
-  final copy = Map.from(dependencies);
-  copy.removeWhere((k, v) => v is! GitReference);
-  return copy
-      .cast<String, GitReference>()
-      .map((k, v) => MapEntry(k, GitDependencyReference(k, location, v)));
-}
-
-Future<void> checkPrs(PubSpec pubspec, GitDependencyReference reference) async {
-  final prUrls =
-      pubspec.unParsedYaml![reference.location.yamlKey][reference.name]['prs'];
-  if (prUrls == null) {
-    print(yellowPen('No PRs listed for ${reference.name}'));
-    return;
+  if (dependencies == null) {
+    return {};
   }
 
-  for (final prUrl in prUrls) {
-    
+  final gitDependencies = <String, GitDependencyReference>{};
+  for (final dependency in dependencies.entries) {
+    final key = dependency.key;
+    final value = dependency.value;
+    if (value is! Map) continue;
+
+    final prs = value['git']?['prs'] as List?;
+    if (prs == null) {
+      continue;
+    }
+    print(prs);
   }
+
+  return gitDependencies;
 }
+
+// Future<void> checkPrs(PubSpec pubspec, GitDependencyReference reference) async {
+//   final prUrls =
+//       pubspec.unParsedYaml![reference.location.yamlKey][reference.name]['prs'];
+//   if (prUrls == null) {
+//     print(yellowPen('No PRs listed for ${reference.name}'));
+//     return;
+//   }
+
+//   for (final prUrl in prUrls) {
+//     final parts = prUrl.split('/');
+//     final owner = parts[parts.length - 3];
+//     final name = parts[parts.length - 2];
+//     final number = parts.last;
+
+//     final pr =
+//         await github.pullRequests.get(RepositorySlug(owner, name), number);
+//     if (pr.state == 'closed') {
+//       print(redPen('PR $prUrl is closed'));
+//     } else {
+//       print(greenPen('PR $prUrl is open'));
+//     }
+//   }
+// }
--- lib/git_dependency_reference.dart
+++ lib/git_dependency_reference.dart
@@ -1,40 +1,14 @@
-import 'package:pubspec/pubspec.dart';
-
 /// A reference to a git dependency
 class GitDependencyReference {
+  /// The location of the dependency in the pubspec
+  final String location;
+
   /// THe package name
   final String name;
 
-  /// The location of the dependency in the pubspec
-  final DependencyLocation location;
-
   /// The git reference
-  final GitReference reference;
+  final List<String> prs;
 
   /// Constructor
-  GitDependencyReference(this.name, this.location, this.reference);
-}
-
-/// The location of a dependency in a pubspec
-enum DependencyLocation {
-  /// The dependencies section
-  dependencies,
-
-  /// The dev_dependencies section
-  devDependencies,
-
-  /// The dependency_overrides section
-  dependencyOverrides;
-
-  /// The yaml key for this location
-  String get yamlKey {
-    switch (this) {
-      case DependencyLocation.dependencies:
-        return 'dependencies';
-      case DependencyLocation.devDependencies:
-        return 'dev_dependencies';
-      case DependencyLocation.dependencyOverrides:
-        return 'dependency_overrides';
-    }
-  }
+  GitDependencyReference(this.location, this.name, this.prs);
 }
