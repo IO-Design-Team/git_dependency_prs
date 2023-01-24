08ff11fa4f8efba58b6f7c0d39135de69bb55bba
Rexios <rexios@rexios.dev>
2023-01-24 17:01:44 -0500

    Initial commit

---
+++ example/git_dep_check_example.dart
@@ -0,0 +1,6 @@
+import 'package:git_dep_check/git_dep_check.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/git_dep_check.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library git_dep_check;
+
+export 'src/git_dep_check_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
